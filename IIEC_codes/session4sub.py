21b0868bd967b929f839d7942f42c41fd4a72aa4
Ayush <43788474+Ayushh10@users.noreply.github.com>
2020-08-20 17:42:56 +0530

    Session 4 submission (IIEC)
    Menu driven program.

---
+++ IIEC_codes/session4sub.py
@@ -0,0 +1,37 @@
+from os import system
+prog = ['chrome', 'notepad', 'wmplayer', 'vlc', 'anaconda', 'jupyter notebook', 'idle']
+ignore, cont = ["not", 'terminate'], True
+#trig = ["run", "launch", "execute", "load"]
+print("Please, enter your name: ", end='')
+name = input().capitalize()
+print(f"Hello, {name}, greetings...\n", end='')
+print('The list of programs: \n')
+for i in prog:
+     print(i, end=', ')
+while cont:   
+     print('\n\n###################################################################\n\nEnter your command: ', end='')
+     command = input().lower()
+     for i in ignore:
+          if i in command.split():
+               print('Command did not run')
+               break   
+          else:
+               for k in prog:
+                    if k in command:
+                         choice = k
+          if 'load' in command or 'run' in command or 'execute' in command or 'launch' in command:
+               if system(choice) == 0:
+                    print(f'{choice} executed')
+                    break
+               else:
+                    print("ERROR!!!, \nUNABLE TO EXECUTE.")
+                    break
+          else:
+               print('COMMAND NOT VALID.')
+               break
+     print('Do you wish to continue? (Y/N): ', end='')
+     cont = input().lower()
+     if cont == 'n':
+          print('Thank You!')
+          cont =False
+
